6ff19497a33fa2b5f4352a069b5e608b18698bb0
admin <opkcloud@163.com>
2019-06-18 17:32:47 +0800

    SSM框架配置修改第1次完整版

--- src/main/java/com/opkcloud/controller/UserController.java
+++ src/main/java/com/opkcloud/controller/UserController.java
@@ -5,18 +5,15 @@ import com.opkcloud.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 /**
  * 搭建教程：https://blog.csdn.net/yeyazhishang/article/details/86650053
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
